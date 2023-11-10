8da05be1b5c17027c4d7ed615ccb63421b9aca8b
Kevin Chen <109507575+kcccr123@users.noreply.github.com>
2023-11-10 18:31:04 -0500

    Initial commit

---
+++ Source/FlightBrowse/FlightBrowseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlightBrowseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLIGHTBROWSE_API AFlightBrowseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
