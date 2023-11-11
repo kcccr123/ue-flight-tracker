f429dbcf365c8188cbec0499b90a9614f4dc65ce
jla818 <jla818@sfu.ca>
2023-11-10 16:58:34 -0800

    api scheduled

--- api/api_to_sql.py
+++ api/api_to_sql.py
@@ -1,7 +1,9 @@
 import pandas as pd
-from FlightRadar24 import FlightRadar24API
 import sqlite3
 from sqlalchemy import create_engine
+from FlightRadar24 import FlightRadar24API
+import schedule
+import time
 
 
 #sqlite setup
@@ -9,36 +11,38 @@ from sqlalchemy import create_engine
 cnx = sqlite3.connect('test.db')
 e = create_engine('sqlite://')
 
-
-
-
-#flight radar dataframe
-df = pd.DataFrame(columns=['origin', 'destination', 'callsign', 'lat', 'lng', 'atd']) 
-
-
 #flight radar config
-fr_api = FlightRadar24API(...)
+fr_api = FlightRadar24API()
 flight_tracker = fr_api.get_flight_tracker_config()
 fr_api.set_flight_tracker_config(flight_tracker)
 
 
-#getting flight data we want and adding it to pandas df
-flights = fr_api.get_flights()
-for flight in flights:
-    #print(flight.origin_airport_iata)
-    #print(flight.destination_airport_iata)
-    #print(flight.aircraft_code)
-    #print(flight.altitude)
-    #print(flight.longitude)
-    #print(flight.latitude)
 
-    df.loc[len(df.index)] = [flight.origin_airport_iata,
-                         flight.destination_airport_iata,
-                         flight.aircraft_code,
-                         flight.latitude,
-                         flight.longitude,
-                         flight.altitude]
+def get_data():
 
-print(df)
 
-df.to_sql(name='flight_data', con=cnx)
+    #flight radar dataframe, reset it to empty
+    df = pd.DataFrame(columns=['origin', 'destination', 'callsign', 'lat', 'lng', 'atd']) 
+
+    #getting flight data we want and adding it to pandas df
+    flights = fr_api.get_flights()
+    for flight in flights:
+        df.loc[len(df.index)] = [flight.origin_airport_iata,
+                            flight.destination_airport_iata,
+                            flight.aircraft_code,
+                            flight.latitude,
+                            flight.longitude,
+                            flight.altitude]
+        
+    print(df.iloc[25])
+    df.to_sql(name='flight_data', con=cnx, if_exists='replace')
+        
+schedule.every(30).seconds.do(get_data)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+
