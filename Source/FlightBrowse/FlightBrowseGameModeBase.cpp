c2d79d0859d001c3f22af69c0f246720ef8aa7bf
Kevin Chen <109507575+kcccr123@users.noreply.github.com>
2023-11-27 06:53:55 -0500

    Remove Useless text

--- Source/FlightBrowse/FlightBrowseGameModeBase.cpp
+++ Source/FlightBrowse/FlightBrowseGameModeBase.cpp
@@ -9,16 +9,6 @@ void AFlightBrowseGameModeBase::StartPlay()
     GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Running"));
     UE_LOG(LogTemp, Log, TEXT("Starting"));
 
-    // Run Python
-    
-    UWorld* World = GetWorld();
-    FString FilePath = TEXT("py" + FPaths::ProjectContentDir() + "Database/api_to_sql.py");
-    UE_LOG(LogTemp, Log, TEXT("Python"));
-    const TCHAR* Path = *FilePath;
-    World->Exec(NULL, Path);
-    UE_LOG(LogTemp, Log, TEXT("Python2"));
-
-
 }
 
 void AFlightBrowseGameModeBase::InitGameState()
